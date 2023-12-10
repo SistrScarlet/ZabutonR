b4eb299cd5ea2da2423a2440bd322e85abf8f869
sistr <game.sistr@gmail.com>
2023-12-10 16:01:57 +0900

    修正：Forge側でうまく動作しないのを修正 ClientSetup以前にRegisterイベントが走るのはギルティ

--- fabric/src/main/java/net/sistr/zabutonr/fabric/ZabutonRFabric.java
+++ fabric/src/main/java/net/sistr/zabutonr/fabric/ZabutonRFabric.java
@@ -1,11 +1,17 @@
 package net.sistr.zabutonr.fabric;
 
+import dev.architectury.registry.client.level.entity.EntityModelLayerRegistry;
+import dev.architectury.registry.client.level.entity.EntityRendererRegistry;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.sistr.zabutonr.ZabutonR;
 import net.fabricmc.api.ModInitializer;
 import net.sistr.zabutonr.ZabutonRClient;
+import net.sistr.zabutonr.client.model.ZabutonEntityModel;
+import net.sistr.zabutonr.client.model.ZabutonEntityModelLayers;
+import net.sistr.zabutonr.client.renderer.ZabutonREntityRenderer;
+import net.sistr.zabutonr.setup.Registration;
 
 public class ZabutonRFabric implements ModInitializer, ClientModInitializer {
     @Override
@@ -16,6 +22,9 @@ public class ZabutonRFabric implements ModInitializer, ClientModInitializer {
     @Environment(EnvType.CLIENT)
     @Override
     public void onInitializeClient() {
+        //Forge側で動作しないため分けて書く
+        EntityRendererRegistry.register(Registration.ZABUTON_ENTITY, ZabutonREntityRenderer::new);
+        EntityModelLayerRegistry.register(ZabutonEntityModelLayers.ZABUTON, ZabutonEntityModel::getTexturedModelData);
         ZabutonRClient.init();
     }
 }
--- forge/src/main/java/net/sistr/zabutonr/forge/ZabutonRForge.java
+++ forge/src/main/java/net/sistr/zabutonr/forge/ZabutonRForge.java
@@ -2,17 +2,22 @@ package net.sistr.zabutonr.forge;
 
 import dev.architectury.platform.forge.EventBuses;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.event.EntityRenderersEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
-import net.sistr.zabutonr.ZabutonR;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.sistr.zabutonr.ZabutonR;
 import net.sistr.zabutonr.ZabutonRClient;
+import net.sistr.zabutonr.client.model.ZabutonEntityModel;
+import net.sistr.zabutonr.client.model.ZabutonEntityModelLayers;
+import net.sistr.zabutonr.client.renderer.ZabutonREntityRenderer;
+import net.sistr.zabutonr.setup.Registration;
 
 @Mod(ZabutonR.MOD_ID)
 public class ZabutonRForge {
     public ZabutonRForge() {
-		// Submit our event bus to let architectury register our content on the right time
+        // Submit our event bus to let architectury register our content on the right time
         EventBuses.registerModEventBus(ZabutonR.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         ZabutonR.init();
     }
@@ -23,6 +28,16 @@ public class ZabutonRForge {
         @SubscribeEvent
         public static void onClientSetup(FMLClientSetupEvent event) {
             ZabutonRClient.init();
+        }
+
+        @SubscribeEvent
+        public static void registerEntityRenderer(EntityRenderersEvent.RegisterRenderers event) {
+            event.registerEntityRenderer(Registration.ZABUTON_ENTITY.get(), ZabutonREntityRenderer::new);
+        }
+
+        @SubscribeEvent
+        public static void registerRenderLayer(EntityRenderersEvent.RegisterLayerDefinitions event) {
+            event.registerLayerDefinition(ZabutonEntityModelLayers.ZABUTON, ZabutonEntityModel::getTexturedModelData);
         }
     }
 
